468eb466d034fcd2dbee6d5ef1fe2d27f426cdbe
v2.kaminsky <v2.kaminsky@itransition.com>
2020-10-25 19:00:59 +0300

    feat: added function toReadeble

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
-module.exports = function toReadable (number) {
-  
+let firstNum = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+let secondNum = ['null', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+let thirdNum = ['null', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+module.exports=function toReadable(number) {
+    if (number === 0) return firstNum[0];
+    if (number % 100 === 0) return (firstNum[number/100]+ ' hundred ').trim();
+
+    let strNum = String(number);
+
+    if (strNum.length === 3) {
+        return convertNum(strNum[0],true)+convertNum(strNum.substr(1,2))
+    }
+
+    if (strNum.length === 1) {
+        return firstNum[Number(strNum)]
+    }
+
+    return convertNum(strNum);
+
+}
+
+function convertNum(num, pos = false) {
+    if (pos) {
+        return firstNum[Number(num)] + ' hundred ';
+    }
+
+    if (Number(num) >= 11 && Number(num) <= 19) {
+        return secondNum[Number(num[1])]
+    }
+
+    if (Number(num) % 10 === 0) {
+        return thirdNum[Number(num/10)]
+    }
+
+    if (Number(num[0]) === 0) {
+        return firstNum[Number(num)]
+    }
+
+    let num1 = num[0],
+        num2 = num[1];
+    return thirdNum[Number(num1)] + ' ' + firstNum[Number(num2)];
 }
